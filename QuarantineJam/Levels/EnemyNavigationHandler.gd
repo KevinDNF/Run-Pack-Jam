8aa9369b685dd980f53d2b6da3efd4badf461d36
azerbijon123 <aaron.r.gregory@gmail.com>
2020-05-02 13:56:05 +0100

    Added fan node

--- QuarantineJam/Levels/EnemyNavigationHandler.gd
+++ QuarantineJam/Levels/EnemyNavigationHandler.gd
@@ -1,29 +1,30 @@
 extends Node
 
-export var maxNumberOfEnemies = 10
-var currentNumberOfEnemies = 0
 var policeman_prefab = preload("res://Enemies/Policeman.tscn")
 var player
 var nav : Navigation2D
+var world
 onready var spawn_position = $SpawnPosition.global_position
 onready var line2d = $Line2D
+
+signal new_policeman_created
 
 #testing, delete after
 var policeman
 
 func _ready() -> void:
+	world = get_node("../../World")
 	player = get_node("../../World/Player")
 	nav = get_node("../../World/Navigation2D")
 
 ## Allows us to spawn police at set intervals
 func _on_PoliceSpawnTimer_timeout() -> void:
-	if currentNumberOfEnemies < maxNumberOfEnemies:
+	if world.currentNumberOfEnemies < world.maxNumberOfEnemies:
 		var new_policeman = policeman_prefab.instance()
 		add_child(new_policeman)
 		new_policeman.global_position = spawn_position
 		new_policeman.nav = nav
 		new_policeman.player = player
-		currentNumberOfEnemies += 1
-		print(currentNumberOfEnemies)
+		emit_signal("new_policeman_created")
 	else:
 		pass
