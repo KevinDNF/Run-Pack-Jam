06236b3e231d72f46506369d405ccfc51124950e
KevinDNF <KevinNavarroFaubla@gmail.com>
2020-05-02 22:07:37 +0100

    fixed out of bound array

--- QuarantineJam/Levels/Cheers.gd
+++ QuarantineJam/Levels/Cheers.gd
@@ -19,7 +19,7 @@ func _ready() -> void:
 	
 func turn_on_crowd_cheering():
 	var pct = playerHud.get_percentage_of_followers()
-	var frame_index = clamp(int(pct * cheerClips.size()), 0, cheerClips.size())
+	var frame_index = clamp(int(pct * cheerClips.size()), 0, cheerClips.size() - 1)
 	cheerClips[frame_index].play()
 
 func turn_off_crowd_cheering():
