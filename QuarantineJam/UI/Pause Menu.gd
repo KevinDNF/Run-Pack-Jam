e7cb986ccf656e1d339d67a0ae2e6807a5d6e265
KevinDNF <KevinNavarroFaubla@gmail.com>
2020-05-03 14:54:28 +0100

    fixed menu resizing + music not stopping

--- QuarantineJam/UI/Pause Menu.gd
+++ QuarantineJam/UI/Pause Menu.gd
@@ -7,21 +7,18 @@ onready var ExitSFX = $ExitSFX
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		toggle_pause()
-		$MenuMusic.play()
-		
 
 func toggle_pause():
+	if not visible:
+		$MenuMusic.play()
+	else:
+		$MenuMusic.stop()
 	visible = not visible
 	get_tree().paused = not get_tree().paused
 
 func _on_Continue_Btn_button_up() -> void:
 	ContinueSFX.play()
-	$MenuMusic.stop()
 	toggle_pause()
 
 func _on_Quit_Btn_button_up() -> void:
-	ExitSFX.play()
-	$MenuMusic.stop()
-	toggle_pause()
-	print("quit not implemented")
 	get_tree().quit()
