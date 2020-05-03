e4f27b149bf5b226039277a5b6c78b18ae59eb7b
KevinDNF <KevinNavarroFaubla@gmail.com>
2020-05-03 14:19:24 +0100

    added base ui, next leve, win

---
+++ QuarantineJam/UI/Next Level Screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+onready var MenuMusic = $MenuMusic
+onready var ContinueSFX = $ContinueSFX
+
+func show():
+	toggle_pause()
+
+func toggle_pause():
+	visible = not visible
+	get_tree().paused = not get_tree().paused
+
+func _on_Button_button_up() -> void:
+	ContinueSFX.play()
+	$MenuMusic.stop()
+	toggle_pause()
+	#add reset level logic
