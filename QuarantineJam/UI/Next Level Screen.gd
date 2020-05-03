737e377853e1a99adfd3d3ef7803f3a7853ec6ea
KevinDNF <KevinNavarroFaubla@gmail.com>
2020-05-03 17:55:05 +0100

    added 4th band memebt + next level screen

--- QuarantineJam/UI/Next Level Screen.gd
+++ QuarantineJam/UI/Next Level Screen.gd
@@ -11,5 +11,7 @@ func toggle_pause():
 	get_tree().paused = not get_tree().paused
 
 func _on_Button_button_up() -> void:
+	Config.current_level += 1
+	get_tree().paused = false
 	get_tree().reload_current_scene()
 	#add reset level logic
--- QuarantineJam/config.gd
+++ QuarantineJam/config.gd
@@ -1,15 +1,21 @@
 extends Node
 
 var STARTING_LEVEL = 0
-var current_level = STARTING_LEVEL
+var current_level = STARTING_LEVEL setget update_level_values
+var MAX_LEVELS = 3
 
 var LEVELS = [
 	"res://Levels/Level1.tscn",
 ]
 
+var maxNumberOfEnemies = 0
+var targetNumberOfFans = 0
 
-var maxNumberOfEnemies = 10
-var currentNumberOfEnemies = 0 
+func _ready() -> void:
+	update_level_values(STARTING_LEVEL)
 
-var targetNumberOfFans = 20
-var currentNumberOfFans = 0 
+func update_level_values(value):
+	current_level = clamp(value, 0, MAX_LEVELS)
+	maxNumberOfEnemies = 2 * (current_level + 1)
+	targetNumberOfFans = 5 * (current_level + 1)
+	print("Updating values...\n" + "Level: " + str(current_level + 1) + "\n" + " Max Enemies: " + str(maxNumberOfEnemies) + "\n" + " Target Fans: " + str(targetNumberOfFans) + "\n")
