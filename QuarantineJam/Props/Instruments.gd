7c43db3fe2567789e30ebd7acac35a119e0e482d
azerbijon123 <aaron.r.gregory@gmail.com>
2020-05-03 20:48:11 +0100

    Added instruments dropping effect and logic

--- QuarantineJam/Props/Instruments.gd
+++ QuarantineJam/Props/Instruments.gd
@@ -1,17 +1,34 @@
 extends KinematicBody2D
 
 onready var softCollision = $SoftCollision
-onready var pickupRadiusCollider = $PickupRadius/CollisionShape2D
+onready var softCollisionCollider = $SoftCollision/CollisionShape2D
+onready var collider = $Collider
+onready var timer = $Timer
 var velocity = Vector2.ZERO
+var target_position
 var id = 0
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
 
 func _ready() -> void:
 	print(id)
 
 func _physics_process(delta: float) -> void:
-	if softCollision.is_colliding():
-		velocity += softCollision.get_push_vector() * delta * 400
-	else:
-		velocity = Vector2.ZERO
-	velocity = move_and_slide(velocity)
 	
+	var direction = position.direction_to(target_position)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	
+	velocity = move_and_slide(velocity)
+#	if softCollision.is_colliding():
+#		velocity += softCollision.get_push_vector() * delta * 400
+#	else:
+#		velocity = Vector2.ZERO
+#	velocity = move_and_slide(velocity)
+	
+
+func _on_Timer_timeout() -> void:
+	softCollisionCollider.disabled = false
+	collider.disabled = false
+	timer.stop()
